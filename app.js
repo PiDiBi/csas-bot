b38edb383c7c68e80eb1f0007ad6519ff6342a3b
David Bures <david@buresovi.com>
2017-02-04 17:16:31 -0800

    server url

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var CSAS_API_KEY = process.env.CSAS_API_KEY;
+var PORT = process.env.port || process.env.PORT || 3978;
+var HOSTNAME = (process.env.WEBSITE_HOSTNAME || "http:/localhost") + ":" + PORT;
 // use demo user : 7777777777 / any password
-// WEB-API-KEY
-// var authCallbackUrl = https://api.csas.cz/sandbox/widp/oauth2/auth?state=profil&redirect_uri=https://api.csas.cz/sandbox/widp/oauth2/redirect&client_id=WebExpoClient&response_type=token
 // token type url
-var authCallbackUrl = 'https://api.csas.cz/sandbox/widp/oauth2/auth?state=profil&redirect_uri=http://localhost:3978/authCallback&client_id=WebExpoClient&response_type=token';
+var authCallbackUrl = 'https://api.csas.cz/sandbox/widp/oauth2/auth?state=profil&redirect_uri='+HOSTNAME+'/authCallback&client_id=WebExpoClient&response_type=token';
 // code type url
 //var authCallbackUrl = 'https://api.csas.cz/sandbox/widp/oauth2/auth?state=profil&redirect_uri=http://localhost:3978/authCallback&client_id=WebExpoClient&response_type=code';
 var authCodeUrl = 'https://api.csas.cz/sandbox/widp/oauth2/auth';
