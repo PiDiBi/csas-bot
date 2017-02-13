68af401db48b19621ee6f55cdb04fbe1ff7577f4
David Bures <david@buresovi.com>
2017-02-13 21:27:46 +0100

    unit testing

---
+++ test/dialog-flows/home.js
@@ -0,0 +1,24 @@
+
+module.exports = [
+  {
+    out: "Hello"
+  },
+  {
+    in: "Hello... I'm a CSAS bank bot."
+  },
+  {    
+    in: "Select (1. Accounts or 2. Cards)",
+    out: "help"
+  },
+  {
+    in: "Select (1. Accounts or 2. Cards)",
+    out : "1"
+  },
+  {
+    in: /^(Select your account)/i,
+    out : "1"
+  },
+  {
+    in: "What do you want to do? Type 'accounts' to return to account selection or 'home' (1. Show balance or 2. Show history)"
+  }
+]
